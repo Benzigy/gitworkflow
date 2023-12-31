2159e1654eaa3fd574257281e0a64921a0944991
Benard Fiifi Sewornu <sewornubernard529@gmail.com>
2023-12-31 11:28:12 +0000

    Completed Arrithmwtic Operations of all Function
    Added subfunction
    Added divfunction
    Added prodfunction
    Added powfunction

--- app.py
+++ app.py
@@ -1,2 +1,14 @@
 def addfunction(x, y):
-    
+    return x+y
+
+def subfunction(x, y):
+    return x-y
+
+def prodfuction(x, y):
+    return x*y
+
+def powfunction(x, y):
+    return x**y
+
+def divfunction(x, y):
+    return x/y
